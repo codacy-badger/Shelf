b853e71c546d85a667171b82c0ef6e476e157c0f
Snailpong <tjdtnsu@gmail.com>
2018-08-03 22:37:48 +0900

    1.0.4

---
+++ app/src/main/java/snailpong/user/allshelf/Message_Item.java
@@ -0,0 +1,23 @@
+package snailpong.user.allshelf;
+
+public class Message_Item {
+
+    private String message_msg;
+    private String message_date;
+
+    public String getMessage_msg() {
+        return message_msg;
+    }
+
+    public void setMessage_msg(String message_msg) {
+        this.message_msg = message_msg;
+    }
+
+    public String getMessage_date() {
+        return message_date;
+    }
+
+    public void setMessage_date(String message_date) {
+        this.message_date = message_date;
+    }
+}
