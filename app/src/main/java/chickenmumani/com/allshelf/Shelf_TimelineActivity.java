5ca3452b223aa4103f925eb55728be8a20c56b4b
Snailpong <tjdtnsu@gmail.com>
2018-08-24 18:36:38 +0900

    1.0.14

---
+++ app/src/main/java/chickenmumani/com/allshelf/Shelf_TimelineActivity.java
@@ -0,0 +1,13 @@
+package chickenmumani.com.allshelf;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Shelf_TimelineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shelf_timeline);
+    }
+}
